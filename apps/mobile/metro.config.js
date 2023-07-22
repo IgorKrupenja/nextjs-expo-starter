14ecb4ae6a6c5f6caaf0a7d491e5d73dd396b63d
Igor Krupenja <igor.krupenja@gmail.com>
2023-07-22 12:10:22 +0300

    WIP watch folders

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -7,8 +7,24 @@ const workspaceRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+// Only list the packages within your monorepo that your app uses. No need to add anything else.
+// If your monorepo tooling can give you the list of monorepo workspaces linked
+// in your app workspace, you can automate this list instead of hardcoding them.
+const monorepoPackages = {
+  shared: path.resolve(workspaceRoot, 'packages/shared'),
+  'eslint-config-custom': path.resolve(workspaceRoot, 'packages/eslint-config-custom'),
+};
+
+// 1. Watch the local app folder, and only the shared packages (limiting the scope and speeding it up)
+// Note how we change this from `workspaceRoot` to `projectRoot`. This is part of the optimization!
+config.watchFolders = [projectRoot, ...Object.values(monorepoPackages)];
+
+// Add the monorepo workspaces as `extraNodeModules` to Metro.
+// If your monorepo tooling creates workspace symlinks in the `node_modules` folder,
+// you can either add symlink support to Metro or set the `extraNodeModules` to avoid the symlinks.
+// See: https://facebook.github.io/metro/docs/configuration/#extranodemodules
+config.resolver.extraNodeModules = monorepoPackages;
+
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
